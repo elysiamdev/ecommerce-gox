d9b59456166ea745535eb05fdbc1c0e2a8e75d13
Lucas <lucasreispedroso@gmail.com>
2019-12-20 21:04:22 -0300

    fix(auth): fix export default

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,10 +1,3 @@
-import { Router, Request, Response } from 'express'
-import services from './services'
+import { verifyToken, ensureToken, signIn, recoveryPassword } from './services'
 
-const router = Router()
-
-router.get('/', (req: Request, res: Response) => res.json({ data: '' }))
-
-export { services };
-
-export default router;
+export default { verifyToken, ensureToken, signIn, recoveryPassword };
