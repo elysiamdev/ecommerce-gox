44e937fb1d0a87cc4367c961f9fcdb06058c556e
bruno <43834700+suika93@users.noreply.github.com>
2019-12-22 23:09:02 -0300

    feat(site): Add login, register, customer info, orders and order info page

--- src/services/site/request_handlers/index.ts
+++ src/services/site/request_handlers/index.ts
@@ -13,3 +13,23 @@ export const contactHandler = (req: any, res: any, next: any) => {
 export const searchHandler = (req: any, res: any, next: any) => {
     res.render('site/search')
 }
+
+export const loginHandler = (req: any, res: any, next: any) => {
+    res.render('site/login')
+}
+
+export const registerHandler = (req: any, res: any, next: any) => {
+    res.render('site/register')
+}
+
+export const orderHistoryHandler = (req: any, res: any, next: any) => {
+    res.render('site/order_history')
+}
+
+export const orderInfoHandler = (req: any, res: any, next: any) => {
+    res.render('site/order_info')
+}
+
+export const customerInfoHandler = (req: any, res: any, next: any) => {
+    res.render('site/customer_info')
+}
--- src/services/site/routes/index.ts
+++ src/services/site/routes/index.ts
@@ -1,5 +1,15 @@
 import express from 'express'
-import { homeHandler, productHandler, contactHandler, searchHandler } from '../request_handlers'
+import { 
+    homeHandler, 
+    productHandler, 
+    contactHandler, 
+    searchHandler, 
+    loginHandler, 
+    registerHandler,
+    orderHistoryHandler,
+    customerInfoHandler,
+    orderInfoHandler
+ } from '../request_handlers'
 
 const router = express.Router()
 
@@ -7,5 +17,12 @@ router.get('/', homeHandler)
 router.get('/produto/:slug', productHandler)
 router.get('/contato', contactHandler)
 router.get('/busca', searchHandler)
+
+router.get('/login', loginHandler)
+router.get('/registrar', registerHandler)
+
+router.get('/pedidos', orderHistoryHandler)
+router.get('/pedidos/:slug', orderInfoHandler)
+router.get('/customer-info', customerInfoHandler)
 
 export default router
