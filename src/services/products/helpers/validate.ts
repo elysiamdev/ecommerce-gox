6cdec1f193a6a99533a784ab3b25f00fc62306dc
Lucas <lucasreispedroso@gmail.com>
2019-12-23 19:11:05 -0300

    fix(import-paths): remove alias accross application

--- src/services/products/helpers/validate.ts
+++ src/services/products/helpers/validate.ts
@@ -40,7 +40,7 @@ const updateOptions = {
 
 const insertOptions = { ...product }
 
-export const validateInsert = (user: any) => validate(user, insertOptions) ? { success: true } : { success: false, data: validate(user, insertOptions) };
+export const validateInsert = (user: any) => validate(user, insertOptions) ? { success: false, data: validate(user, insertOptions) } : { success: true };
 
 export const validateUpdate = (user: any) => validate(user, updateOptions) ? { success: false, data: validate(user, updateOptions) } : { success: true };
 
