41437cde9de836663e3900a49d35e672c482953c
bruno <43834700+suika93@users.noreply.github.com>
2019-12-26 14:18:14 -0300

    feat(react-components): Prepare structure for ProductCarrousel component

--- src/services/site/request_handlers/index.ts
+++ src/services/site/request_handlers/index.ts
@@ -1,5 +1,25 @@
 export const homeHandler = (req: any, res: any, next: any) => {
-    res.render('site/home')
+    const sharedData = JSON.stringify({
+        "slides": [
+            { title: "Slider 1", products: [
+                { title: 'Produto 1', url: '/site/produto/produto-1', image: undefined, price: 12.01 },
+                { title: 'Produto 2', url: '/site/produto/produto-2', image: undefined, price: 14.01 },
+                { title: 'Produto 3', url: '/site/produto/produto-3', image: undefined, price: 16.01 },
+                { title: 'Produto 4', url: '/site/produto/produto-4', image: undefined, price: 16.01 },
+                { title: 'Produto 5', url: '/site/produto/produto-5', image: undefined, price: 16.01 },
+                { title: 'Produto 6', url: '/site/produto/produto-6', image: undefined, price: 16.01 },
+            ]},
+            { title: "Slider 2", products: [
+                { title: 'Produto 7', url: '/site/produto/produto-7', image: undefined, price: 16.01 },
+                { title: 'Produto 8', url: '/site/produto/produto-8', image: undefined, price: 16.01 },
+                { title: 'Produto 9', url: '/site/produto/produto-9', image: undefined, price: 16.01 },
+                { title: 'Produto 10', url: '/site/produto/produto-10', image: undefined, price: 16.01 },
+                { title: 'Produto 11', url: '/site/produto/produto-11', image: undefined, price: 16.01 },
+                { title: 'Produto 12', url: '/site/produto/produto-12', image: undefined, price: 16.01 },
+            ]}
+    ]})
+
+    res.render('site/home', { sharedData: sharedData })
 }
 
 export const productHandler = (req: any, res: any, next: any) => {
