59f11295b25b58a1b0af248e0e0d012f58cd4891
bruno <847754-soobak93@users.noreply.gitlab.com>
2020-01-30 14:48:35 -0300

    chore: Remove outdated code

--- src/services/site/request_handlers/index.ts
+++ src/services/site/request_handlers/index.ts
@@ -6,34 +6,6 @@ import { AddressRepository } from "@services/users/db/repositories"
 
 export const homeHandler = (req: any, res: any, next: any) => {
     const sharedData = JSON.stringify({
-        "slides": [
-            {
-                title: "Slider 1", products: [
-                    { title: 'Produto 1', url: '/site/produto/produto-1', image: undefined, price: 12.01 },
-                    { title: 'Produto 2', url: '/site/produto/produto-2', image: undefined, price: 14.01 },
-                    { title: 'Produto 3', url: '/site/produto/produto-3', image: undefined, price: 16.01 },
-                    { title: 'Produto 4', url: '/site/produto/produto-4', image: undefined, price: 16.01 },
-                    { title: 'Produto 5', url: '/site/produto/produto-5', image: undefined, price: 16.01 },
-                    { title: 'Produto 6', url: '/site/produto/produto-6', image: undefined, price: 16.01 },
-                ]
-            },
-            {
-                title: "Slider 2", products: [
-                    { title: 'Produto 7', url: '/site/produto/produto-7', image: undefined, price: 16.01 },
-                    { title: 'Produto 8', url: '/site/produto/produto-8', image: undefined, price: 16.01 },
-                    { title: 'Produto 9', url: '/site/produto/produto-9', image: undefined, price: 16.01 },
-                    { title: 'Produto 10', url: '/site/produto/produto-10', image: undefined, price: 16.01 },
-                    { title: 'Produto 11', url: '/site/produto/produto-11', image: undefined, price: 16.01 },
-                    { title: 'Produto 12', url: '/site/produto/produto-12', image: undefined, price: 16.01 },
-                ]
-            }
-        ],
-        "cart": {
-            "products": [
-                { id: 1, title: 'Produto 1', url: '/site/produto/produto-1', image: undefined, price: 12.01, quantity: 1 },
-            ]
-        },
-        "slides_wide": {
         "cart": {
             "products": [
                 { id: 1, title: 'Produto 1', url: '/produto/produto-1', image: undefined, price: 12.01, quantity: 1 },
@@ -49,7 +21,7 @@ export const homeHandler = (req: any, res: any, next: any) => {
             ]
         },
         "deals": {
-            "dealsx": {
+            "dealsx": { 
                 title: "Deals title", products: [
                     { id: 1, title: "Deals product 1", sale: false, image: undefined, url: "/url-1", oldPrice: 15.01, newPrice: 20.01, description: "A product 1" },
                     { id: 2, title: "Deals product 2", sale: -20, image: undefined, url: "/url-2", oldPrice: 16.01, newPrice: 20.02, description: "A product 2" },
@@ -105,27 +77,6 @@ export const loginHandler = (req: any, res: any, next: any) => {
 
 export const registerHandler = (req: any, res: any, next: any) => {
     res.render('site/register')
-}
-
-export const register2Handler = async (req: any, res: any, next: any) => {
-    const unitOfWork = new UnitOfWork(knex)
-    await unitOfWork.initialize()
-
-    const registerRequestModel = mapRegisterRequestBodyIntoRegisterRequestModel(req.body)
-    const createUserProfile = makeCreateUserProfile(unitOfWork.getUserProfileRepository())
-    const createUserCredentials = makeCreateUserLocalCredentials(unitOfWork.getUserLocalCredentialsRepository(), hashPassword)
-    const registerUser = makeRegisterUser(createUserProfile, createUserCredentials)
-
-    const user = await registerUser(registerRequestModel)
-
-    if (user) {
-        unitOfWork.commit()
-        res.status(201).send(user)
-    }
-    else {
-        unitOfWork.rollback()
-        next(new Error('Idk yet'))
-    }
 }
 
 export const orderHistoryHandler = (req: any, res: any, next: any) => {
