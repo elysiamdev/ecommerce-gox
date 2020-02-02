2bc61a859f77d4d34635599230fdd498a5e3a47f
bruno <43834700+suika93@users.noreply.github.com>
2020-02-02 19:27:31 -0300

    fix(cart): Fix cart items not showing in '/minha-conta'

--- src/services/my_account/request_handlers/index.ts
+++ src/services/my_account/request_handlers/index.ts
@@ -2,22 +2,34 @@ import { makeFindUserProfileById } from "@services/users/services/findUserProfil
 import { UserProfileRepository, AddressRepository } from "@services/users/db/repositories"
 import knex from "@database/index"
 import { makeFindUserAddresses } from "@services/users/services/findUserAddresses"
+import useCart from "@services/cart/middleware/useCart"
+import { SharedData } from "@logic/SharedData"
 
-export const customerInfoHandler = async (req: any, res: any, next: any) => {
+export const customerInfoHandler = useCart(async (req: any, res: any, next: any) => {
+    const sd = new SharedData()
+    sd.add("cart", { products: req.user_cart.items })
+
     const findUserProfileById = makeFindUserProfileById(new UserProfileRepository(knex))
     const profile = await findUserProfileById(req.user.id)
     
-    res.render('my_account/home/index', { profile })
-}
+    res.render('my_account/home/index', { profile, sharedData: sd.toJson() })
+})
 
-export const listAddressesHandler = async (req: any, res: any, next: any) => {
+export const listAddressesHandler = useCart(async (req: any, res: any, next: any) => {
     const findUserAddresses = makeFindUserAddresses(new AddressRepository(knex))
     const userId = req.user.id
     const addresses = await findUserAddresses(userId)
 
-    res.render('my_account/addresses/index', { sharedData: JSON.stringify({addresses: addresses}) })
-}
+    const sd = new SharedData()
+    sd.add("cart", { products: req.user_cart.items })
+    sd.add("addresses", addresses)
 
-export const newAddressesHandler = async (req: any, res: any, next: any) => {
-    res.render('my_account/addresses/new', { addresses: []})
-}
+    res.render('my_account/addresses/index', { sharedData: sd.toJson() })
+})
+
+export const newAddressesHandler = useCart(async (req: any, res: any, next: any) => {
+    const sd = new SharedData()
+    sd.add("cart", { products: req.user_cart.items })
+
+    res.render('my_account/addresses/new', { addresses: [], sharedData: sd.toJson() })
+})
