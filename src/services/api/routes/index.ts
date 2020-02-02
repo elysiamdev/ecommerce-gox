299c008a8f01156393e5c3f4f07a25e5b8100bd6
bruno <43834700+suika93@users.noreply.github.com>
2020-02-02 14:04:43 -0300

    feat(cart): Add cart endpoints to the API

--- src/services/api/routes/index.ts
+++ src/services/api/routes/index.ts
@@ -2,13 +2,16 @@ import { Router } from 'express'
 import { registerHandler, createAddressHandler, deleteAddressHandler } from '@services/users/request_handlers'
 import { protectHandler } from '@services/auth/middlewares/protectHandler'
 import { checkoutHandler } from '@services/checkout/request_handlers'
+import { addToCartHandler, removeProductFromCartHandler, updateProductQuantityHandler } from '@services/cart/request_handlers'
 
 const router = Router()
 
 router.post('/v1/register', registerHandler)
 router.post('/v1/addresses', protectHandler, createAddressHandler)
 router.delete('/v1/addresses/:id', protectHandler, deleteAddressHandler)
-
+router.post('/v1/cart', addToCartHandler)
+router.delete('/v1/cart/:id', removeProductFromCartHandler)
+router.post('/v1/cart/product/:id', updateProductQuantityHandler)
 router.post('/v1/checkout', checkoutHandler)
 
 export default router
